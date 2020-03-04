e119a04e652fe65877547fd1713b704afa8aacee
Hilton Meyer <development@hiltonmeyer.com>
2020-03-04 19:04:23 +0200

    Move html from index to Feather.vue

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Feather/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/Feather.vue'
+import Feather from './components/Feather.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Feather
   }
 }
+
+
 </script>
 
 <style>
---
+++ src/components/Feather.vue
@@ -0,0 +1,69 @@
+<template>
+  <div id="app">
+    <div style="scale">
+      <div class="icon">
+        <img src="../assets/icons8-scale-64.png" alt="Scale Icon" />
+      </div>
+    </div>
+    <header>
+      <div>
+        <button
+          id="btn-login"
+          class="min-w-1/12 bg-transparent hover:bg-green-500 text-green-700 font-semibold uppercase hover:text-white py-2 px-4 border border-green-500 hover:border-transparent rounded text-sm"
+        >
+          Log in
+        </button>
+        <button
+          id="btn-logout"
+          class="min-w-1/12 bg-transparent hover:bg-green-500 text-green-700 font-semibold uppercase hover:text-white py-2 px-4 border border-green-500 hover:border-transparent rounded text-sm"
+        >
+          Log out
+        </button>
+      </div>
+    </header>
+    <button
+      id="add-weight"
+      @click="showModal = true"
+      class="min-w-1/12 bg-transparent hover:bg-green-500 text-green-700 font-semibold uppercase hover:text-white py-2 px-4 border border-green-500 hover:border-transparent rounded text-sm"
+    >
+      Add Weight
+    </button>
+    <modal v-if="showModal" @close="showModal = false"></modal>
+    <div class="graph">
+      <div id="chart-area"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+
+.scale {
+  position: absolute;
+  left: 50%;
+  top: 0.5rem;
+}
+</style>
