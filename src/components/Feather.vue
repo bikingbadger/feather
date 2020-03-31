1934e87629aaba14c531dc4be7ad5d62f606790f
Hilton Meyer <development@hiltonmeyer.com>
2020-03-31 19:16:03 +0300

    Show and hide modal

--- src/components/Feather.vue
+++ src/components/Feather.vue
@@ -7,28 +7,12 @@
     </div>
     <header>
       <div>
-        <button
-          id="btn-login"
-          class="min-w-1/12 bg-transparent hover:bg-green-500 text-green-700 font-semibold uppercase hover:text-white py-2 px-4 border border-green-500 hover:border-transparent rounded text-sm"
-        >
-          Log in
-        </button>
-        <button
-          id="btn-logout"
-          class="min-w-1/12 bg-transparent hover:bg-green-500 text-green-700 font-semibold uppercase hover:text-white py-2 px-4 border border-green-500 hover:border-transparent rounded text-sm"
-        >
-          Log out
-        </button>
-        <button
-          id="add-weight"
-          @click="showModal = true"
-          class="min-w-1/12 bg-transparent hover:bg-green-500 text-green-700 font-semibold uppercase hover:text-white py-2 px-4 border border-green-500 hover:border-transparent rounded text-sm"
-        >
-          Add Weight
-        </button>
+        <button id="btn-login">Log in</button>
+        <button id="btn-logout">Log out</button>
+        <button id="add-weight" @click="showModal()">Add Weight</button>
       </div>
     </header>
-    <Weight v-if="showModal" @close="showModal = false"></Weight>
+    <Weight v-if="openModal" v-on:hideModal="hideModal()"></Weight>
     <div class="graph">
       <div id="chart-area"></div>
     </div>
@@ -36,42 +20,26 @@
 </template>
 
 <script>
-import Weight from "./Weight.vue";
+import Weight from './Weight.vue';
 
 export default {
-  name: "HelloWorld",
-  props: {
-    msg: String,
-  },
+  name: 'Feather',
   components: {
     Weight,
   },
   data: () => ({
-    showModal: false,
+    openModal: false,
   }),
+  methods: {
+    showModal: function() {
+      this.openModal = true;
+    },
+    hideModal: function() {
+      this.openModal = false;
+    }
+  },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-/* h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-
-.scale {
-  position: absolute;
-  left: 50%;
-  top: 0.5rem;
-} */
-</style>
+<style scoped></style>
--- src/components/Weight.vue
+++ src/components/Weight.vue
@@ -1,45 +1,90 @@
 <template>
-  <form action>
-    <div class="input-item-col">
-      <label for="weight-date">Date</label>
-      <input type="date" name="weight-date" id="weight-date" />
+  <!-- opacity-0 pointer-events-none  -->
+  <div
+    class="modal absolute w-full h-full top-0 left-0 flex items-center justify-center"
+  >
+    <div
+      class="modal-overlay absolute w-full h-full bg-black opacity-25 top-0 left-0 cursor-pointer"
+    ></div>
+
+    <div class="absolute md:w-1/2 py-8 bg-white rounded-lg shadow-lg md:text-xl grid justify-center gap-4">
+      <div class="grid grid-cols-3 grid-flow-col gap-4 mx-4">
+        <label class="grid justify-start py-2" for="weight-date">Date</label>
+        <input  class="bg-green-200 rounded p-2 col-span-2" type="date" name="weight-date" id="weight-date" />
+      </div>
+      <div class="grid grid-cols-3 grid-flow-col gap-4 mx-4">
+        <label class="grid justify-start py-2" for="weight">Enter weight?</label>
+        <input class="bg-green-200 rounded p-2 col-span-2"
+          type="number"
+          step="0.1"
+          min="0"
+          name="weight-kilograms"
+          id="weight-kilograms"
+        />
+      </div>
+      <div class="grid grid-cols-3 grid-flow-col gap-4 mx-4">
+        <label class="grid justify-start py-2" for="fat-perc">Fat %</label>
+        <input  class="bg-green-200 rounded p-2 col-span-2" type="number" step="0.1" min="0" name="fat-perc" id="fat-perc" />
+      </div>
+      <div class="grid grid-cols-3 grid-flow-col gap-4 mx-4">
+        <label class="grid justify-start py-2" for="weight-bone">Bone Weight</label>
+        <input class="bg-green-200 rounded p-2 col-span-2"
+          type="number"
+          step="0.1"
+          min="0"
+          name="weight-bone"
+          id="weight-bone"
+        />
+      </div>
+      <div class="grid grid-cols-3 grid-flow-col gap-4 mx-4">
+        <label class="grid justify-start py-2" for="water-perc">Water %</label>
+        <input class="bg-green-200 rounded p-2 col-span-2"
+          type="number"
+          step="0.1"
+          min="0"
+          name="water-perc"
+          id="water-perc"
+        />
+      </div>
+      <div class="grid grid-cols-3 grid-flow-col gap-4 mx-4">
+        <label class="grid justify-start py-2" for="weight-muscle">Muscle</label>
+        <input class="bg-green-200 rounded p-2 col-span-2"
+          type="number"
+          step="0.1"
+          min="0"
+          name="weight-muscle"
+          id="weight-muscle"
+        />
+      </div>
+      <div class="grid grid-cols-3 grid-flow-col gap-4 mx-4">
+        <label class="grid justify-start py-2" for="belly-index">Belly Index</label>
+        <input class="bg-green-200 rounded p-2 col-span-2"
+          type="number"
+          step="0.1"
+          min="0"
+          name="belly-index"
+          id="belly-index"
+        />
+      </div>
+      <div class="button-item span-across">
+        <button @click="closeModal"
+          type="submit">
+          Add weight
+        </button>
+      </div>
     </div>
-    <div class="input-item-col">
-      <label for="weight">Enter weight?</label>
-      <input type="number" step="0.1" min="0" name="weight-kilograms" id="weight-kilograms" />
-    </div>
-    <div class="input-item-col">
-      <label for="fat-perc">Fat %</label>
-      <input type="number" step="0.1" min="0" name="fat-perc" id="fat-perc" />
-    </div>
-    <div class="input-item-col">
-      <label for="weight-bone">Bone Weight</label>
-      <input type="number" step="0.1" min="0" name="weight-bone" id="weight-bone" />
-    </div>
-    <div class="input-item-col">
-      <label for="water-perc">Water %</label>
-      <input type="number" step="0.1" min="0" name="water-perc" id="water-perc" />
-    </div>
-    <div class="input-item-col">
-      <label for="weight-muscle">Muscle</label>
-      <input type="number" step="0.1" min="0" name="weight-muscle" id="weight-muscle" />
-    </div>
-    <div class="input-item-col">
-      <label for="belly-index">Belly Index</label>
-      <input type="number" step="0.1" min="0" name="belly-index" id="belly-index" />
-    </div>
-    <div class="button-item span-across">
-      <button
-        type="submit"
-        class="min-w-1/12 bg-transparent hover:bg-green-500 text-green-700 font-semibold uppercase hover:text-white py-2 px-4 border border-green-500 hover:border-transparent rounded text-sm"
-      >Add weight</button>
-    </div>
-  </form>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  methods: {
+    closeModal: function () {
+      console.log('Close Modal');
+      this.$emit('hideModal');
+    }
+  }
+};
 </script>
 
-<style>
-</style>
+<style></style>
