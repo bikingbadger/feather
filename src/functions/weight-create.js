f4110e96947a879136ca74acc364424e8212706d
Hilton Meyer <development@hiltonmeyer.com>
2020-05-11 12:37:52 +0300

    Create weight in faunadb

--- src/functions/weight-create.js
+++ src/functions/weight-create.js
@@ -10,35 +10,29 @@ const client = new faunadb.Client({
 /* export our lambda function as named "handler" export */
 exports.handler = (event, context, callback) => {
   /* parse the string body into a useable JS object */
-  console.log('body:',event.body);
-//   if (!event.body.weight) {
-//     return callback(null, {
-//       statusCode: 500,
-//       body: JSON.stringify('Expected weight object'),
-//     });
-//   }
-//   const data = JSON.parse(event.body);
-  
-  //   const todoItem = {
-  //     data: data,
-  //   };
-  //   /* construct the fauna query */
-  //   return client
-  //     .query(q.Create(q.Ref('indexes/all_weight'), todoItem))
-  //     .then((response) => {
-  //       console.log('success', response);
-  //       /* Success! return the response with statusCode 200 */
-  //       return callback(null, {
-  //         statusCode: 200,
-  //         body: JSON.stringify(response),
-  //       });
-  //     })
-  //     .catch((error) => {
-  //       console.log('error', error);
-  //       /* Error! return the error with statusCode 400 */
-  //       return callback(null, {
-  //         statusCode: 400,
-  //         body: JSON.stringify(error),
-  //       });
-  //     });
+  console.log('======================================================');
+  console.log(event);
+  console.log(context);
+  console.log('======================================================');
+  const weight = { data: JSON.parse(event.body) };
+
+  /* construct the fauna query */
+  return client
+    .query(q.Create(q.Ref('classes/weight'), weight))
+    .then((response) => {
+      console.log('success', response);
+      /* Success! return the response with statusCode 200 */
+      return callback(null, {
+        statusCode: 201,
+        body: JSON.stringify(weight.data),
+      });
+    })
+    .catch((error) => {
+      console.log('error', error);
+      /* Error! return the error with statusCode 400 */
+      return callback(null, {
+        statusCode: 400,
+        body: JSON.stringify(error),
+      });
+    });
 };
