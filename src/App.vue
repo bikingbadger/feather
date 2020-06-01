b9ee9e4ef6f8527e278bf4a41a4fd3b3a881fa03
Hilton Meyer <development@hiltonmeyer.com>
2020-06-01 10:41:58 +0300

    Add global header to app

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,17 @@
 <template>
-  <div id="app" class="container mx-auto">
-    <Feather />
+  <div id="app">
+    <div class="grid grid-cols-1">
+      <div class="grid justify-center">
+        <img src="@/assets/icons8-scale-64.png" alt="Scale Icon" />
+      </div>
+    </div>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/weight">Weight</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
-
-<script>
-import Feather from './components/Feather.vue';
-
-export default {
-  name: 'App',
-  components: {
-    Feather,
-  },
-};
-</script>
 
 <style>
 #app {
@@ -22,6 +20,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
