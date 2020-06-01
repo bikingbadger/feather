f045f33ba4435dca5f6a87f821a785d72f13da7b
Hilton Meyer <development@hiltonmeyer.com>
2020-06-01 13:48:31 +0300

    Fill empty days in the array

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -57,7 +57,7 @@ export default {
   async created() {
     // Fetch the data via API and convert response to json
     const resp = await fetch(this.url);
-    console.log(resp);
+    // console.log(resp);
     // Store the json values into the
     this.weightData = await resp.json();
     // console.log(data);
@@ -91,10 +91,65 @@ export default {
       return 0;
     });
 
-    console.log();
+    /**
+     * Fill the array with empty dates where a weight has not been input
+     * This is to give the graph x-axis the correct number of days
+     */
+    // Find the first date and the last date in the array of wights
+    let currentDate = new Date(this.weightData[0].weightDate);
+    let maxDate = new Date(
+      this.weightData[this.weightData.length - 1].weightDate,
+    );
+    // The counter is used for finding current index used to look up the array
+    // This is updated inside the loop automatically but the initial values move
+    //   on as empty dates are pushed on.
+    let counter = 0;
+    // The current weight is added to the empty value and updated once a weight entry
+    //   is found. That way the weight stays the same during that time creating a flat
+    //   line over time. This I found was the best way to get around the empty days.
+    let currentWeight = this.weightData[0].weightKilograms;
+    // The loop is between the first date and the last
+    while (currentDate <= maxDate) {
+      // Use the counter to set the date
+      const weightDate = new Date(this.weightData[counter].weightDate);
+      // Check the values of the currentDate to the one in the array
+      // If the dates are not equal then push an empty value into the array
+      // This fills the array with the current dates in the loop where there are
+      //   no values in the weight data
+      if (weightDate.valueOf() !== currentDate.valueOf()) {
+        // Create the empty date for consistent look
+        // The month and date need to be padded for single digit numbers
+        const emptyYear = currentDate.getFullYear();
+        const emptyMonth = currentDate
+          .getMonth()
+          .toString()
+          .padStart(2, '0');
+        const emptyDate = currentDate
+          .getDate()
+          .toString()
+          .padStart(2, '0');
+        const emptyDateFull = `${emptyYear}-${emptyMonth}-${emptyDate}`;
+        // Insert the empty weight using splice and the counter. This fills the array
+        this.weightData.splice(counter, 0, {
+          weightDate: emptyDateFull,
+          weightKilograms: currentWeight,
+        });
+      } else {
+        // If the date is found and is equal then set the current weight
+        //   for the next empty values if there are any
+        currentWeight = this.weightData[counter].weightKilograms;
+      }
 
+      // Increase the counter
+      counter++;
+      // Set the date to the next day
+      currentDate.setDate(currentDate.getDate() + 1);
+    }
+    
+    // Set the values in the graph by looping over the array with 
+    // the filled days
     this.weightData.forEach((value) => {
-      // console.log(value.weightDate);
+      // console.log('Weight', value.weightDate);
       this.pvOptions.xaxis.categories.push(value.weightDate);
       this.pvSeries[0].data.push(value.weightKilograms);
     });
