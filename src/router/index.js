72c6f9a19cb2be3eb8ec98e496b7e2f721eda5fa
Hilton Meyer <development@hiltonmeyer.com>
2020-06-01 10:41:26 +0300

    Setup router

---
+++ src/router/index.js
@@ -0,0 +1,28 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+  const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: () => import(/* webpackChunkName: "home" */ '@/components/Home.vue')
+  },
+  {
+    path: '/weight',
+    name: 'Weight',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "weight" */ '@/components/Weight.vue')
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
