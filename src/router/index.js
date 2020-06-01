ca1845f587b57a02f911c1d2a8e7f239fc301ca0
Hilton Meyer <development@hiltonmeyer.com>
2020-06-01 10:55:52 +0300

    Added default / route

--- src/router/index.js
+++ src/router/index.js
@@ -4,14 +4,15 @@ import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
   const routes = [
+    { path: "/", redirect: "/home" },
   {
-    path: '/',
-    name: 'Home',
+    path: '/home',
+    name: 'home',
     component: () => import(/* webpackChunkName: "home" */ '@/components/Home.vue')
   },
   {
     path: '/weight',
-    name: 'Weight',
+    name: 'weight',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
