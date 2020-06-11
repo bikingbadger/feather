c1ffd190d1d0efd763f2ff02ef3bb3893d753a78
Hilton Meyer <development@hiltonmeyer.com>
2020-06-11 13:36:20 +0300

    Convert read all to graphql

--- src/functions/weight-read-all.js
+++ src/functions/weight-read-all.js
@@ -1,36 +1,37 @@
 require('dotenv').config();
-import faunadb from 'faunadb';
-
-const q = faunadb.query;
-const client = new faunadb.Client({
-  secret: process.env.FAUNADB_SECRET,
-});
+const axios = require('axios');
 
 exports.handler = (event, context, callback) => {
   console.log(`${event.httpMethod}: ${event.path}`);
-  return client
-    .query(q.Paginate(q.Match(q.Ref('indexes/all_weight'))))
-    .then((response) => {
-      const references = response.data;
-      // console.log('references', references);
-      const getAllDataQuery = references.map((ref) => {
-        // console.log(ref);
-        return q.Get(ref);
-      });
-      // then query the refs
-      return client.query(getAllDataQuery).then((ret) => {
-        console.log(ret);
-        const weightSeries = ret.map(data => {
-          return data.data;
-        })
-        return callback(null, {
-          statusCode: 200,
-          body: JSON.stringify(weightSeries),
-        });
+  return axios({
+    url: 'https://graphql.fauna.com/graphql',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+      Authorization: `Bearer ${process.env.FAUNADB_SECRET}`,
+    },
+    method: 'post',
+    data: {
+      query: `
+        query FindAllWeights {
+          allWeights {
+            data {
+              weightDate
+              weightKilograms
+            }
+          }
+        }
+        `,
+    },
+  })
+    .then((result) => {
+      console.log(result.data);
+      return callback(null, {
+        statusCode: 200,
+        body: JSON.stringify(result.data),
       });
     })
     .catch((error) => {
-      console.log('error', error);
       return callback(null, {
         statusCode: 400,
         body: JSON.stringify(error),
