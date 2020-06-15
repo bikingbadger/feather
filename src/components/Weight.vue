5c4fa45b54bd0ea48e83a6b92b01518bddcbca72
Hilton Meyer <development@hiltonmeyer.com>
2020-06-15 13:37:07 +0300

    Fix color

--- src/components/Weight.vue
+++ src/components/Weight.vue
@@ -1,104 +1,102 @@
 <template>
   <!-- opacity-0 pointer-events-none  -->
-   <div class="grid justify-center p-4 mx-32">
-    <div
+  <div
+    class="grid justify-center md:border-2 rounded border-blue-500 p-4 md:mx-32 gap-4 text-sm md:text-xl"
+  >
+    <!-- <div
       class="absolute md:w-1/2 py-8 bg-white rounded-lg shadow-lg md:text-xl grid justify-center gap-4"
-    >
-      <div class="grid grid-cols-3 grid-flow-col gap-4 mx-4">
-        <label class="grid justify-start py-2" for="weight-date">Date</label>
-        <input
-          class="bg-green-200 rounded p-2 col-span-2"
-          type="date"
-          name="weight-date"
-          id="weight-date"
-          v-model="weightDate"
-        />
-      </div>
-      <div class="grid grid-cols-3 grid-flow-col gap-4 mx-4">
-        <label class="grid justify-start py-2" for="weight"
-          >Enter weight?</label
-        >
-        <input
-          class="bg-green-200 rounded p-2 col-span-2"
-          type="number"
-          step="0.1"
-          min="0"
-          name="weight-kilograms"
-          id="weight-kilograms"
-          v-model="weightKilograms"
-        />
-      </div>
-      <div class="grid grid-cols-3 grid-flow-col gap-4 mx-4">
-        <label class="grid justify-start py-2" for="fat-perc">Fat %</label>
-        <input
-          class="bg-green-200 rounded p-2 col-span-2"
-          type="number"
-          step="0.1"
-          min="0"
-          name="fat-perc"
-          id="fat-perc"
-          v-model="fatPercent"
-        />
-      </div>
-      <div class="grid grid-cols-3 grid-flow-col gap-4 mx-4">
-        <label class="grid justify-start py-2" for="weight-bone"
-          >Bone Weight</label
-        >
-        <input
-          class="bg-green-200 rounded p-2 col-span-2"
-          type="number"
-          step="0.1"
-          min="0"
-          name="weight-bone"
-          id="weight-bone"
-          v-model="weightBone"
-        />
-      </div>
-      <div class="grid grid-cols-3 grid-flow-col gap-4 mx-4">
-        <label class="grid justify-start py-2" for="water-perc">Water %</label>
-        <input
-          class="bg-green-200 rounded p-2 col-span-2"
-          type="number"
-          step="0.1"
-          min="0"
-          name="water-perc"
-          id="water-perc"
-          v-model="waterPercent"
-        />
-      </div>
-      <div class="grid grid-cols-3 grid-flow-col gap-4 mx-4">
-        <label class="grid justify-start py-2" for="weight-muscle"
-          >Muscle</label
-        >
-        <input
-          class="bg-green-200 rounded p-2 col-span-2"
-          type="number"
-          step="0.1"
-          min="0"
-          name="weight-muscle"
-          id="weight-muscle"
-          v-model="weightMuscle"
-        />
-      </div>
-      <div class="grid grid-cols-3 grid-flow-col gap-4 mx-4">
-        <label class="grid justify-start py-2" for="belly-index"
-          >Belly Index</label
-        >
-        <input
-          class="bg-green-200 rounded p-2 col-span-2"
-          type="number"
-          step="0.1"
-          min="0"
-          name="belly-index"
-          id="belly-index"
-          v-model="bellyIndex"
-        />
-      </div>
-      <div class="button-item span-across">
-        <button @click="createWeight">
-          Add weight
-        </button>
-      </div>
+    > -->
+    <div class="grid grid-cols-3 grid-flow-col gap-4 mx-4">
+      <label class="grid justify-start py-2" for="weight-date">Date</label>
+      <in
+        put
+        class="bg-blue-100 rounded p-2 col-span-2"
+        type="date"
+        name="weight-date"
+        id="weight-date"
+        v-model="weightDate"
+      />
+    </div>
+    <div class="grid grid-cols-3 grid-flow-col gap-4 mx-4">
+      <label class="grid justify-start py-2" for="weight">Enter weight?</label>
+      <input
+        class="bg-blue-100 rounded p-2 col-span-2"
+        type="number"
+        step="0.1"
+        min="0"
+        name="weight-kilograms"
+        id="weight-kilograms"
+        v-model="weightKilograms"
+      />
+    </div>
+    <div class="grid grid-cols-3 grid-flow-col gap-4 mx-4">
+      <label class="grid justify-start py-2" for="fat-perc">Fat %</label>
+      <input
+        class="bg-blue-100 rounded p-2 col-span-2"
+        type="number"
+        step="0.1"
+        min="0"
+        name="fat-perc"
+        id="fat-perc"
+        v-model="fatPercent"
+      />
+    </div>
+    <div class="grid grid-cols-3 grid-flow-col gap-4 mx-4">
+      <label class="grid justify-start py-2" for="weight-bone"
+        >Bone Weight</label
+      >
+      <input
+        class="bg-blue-100 rounded p-2 col-span-2"
+        type="number"
+        step="0.1"
+        min="0"
+        name="weight-bone"
+        id="weight-bone"
+        v-model="weightBone"
+      />
+    </div>
+    <div class="grid grid-cols-3 grid-flow-col gap-4 mx-4">
+      <label class="grid justify-start py-2" for="water-perc">Water %</label>
+      <input
+        class="bg-blue-100 rounded p-2 col-span-2"
+        type="number"
+        step="0.1"
+        min="0"
+        name="water-perc"
+        id="water-perc"
+        v-model="waterPercent"
+      />
+    </div>
+    <div class="grid grid-cols-3 grid-flow-col gap-4 mx-4">
+      <label class="grid justify-start py-2" for="weight-muscle">Muscle</label>
+      <input
+        class="bg-blue-100 rounded p-2 col-span-2"
+        type="number"
+        step="0.1"
+        min="0"
+        name="weight-muscle"
+        id="weight-muscle"
+        v-model="weightMuscle"
+      />
+    </div>
+    <div class="grid grid-cols-3 grid-flow-col gap-4 mx-4">
+      <label class="grid justify-start py-2" for="belly-index"
+        >Belly Index</label
+      >
+      <input
+        class="bg-blue-100 rounded p-2 col-span-2"
+        type="number"
+        step="0.1"
+        min="0"
+        name="belly-index"
+        id="belly-index"
+        v-model="bellyIndex"
+      />
+    </div>
+    <div class="button-item span-across">
+      <button @click="createWeight">
+        Add weight
+      </button>
     </div>
   </div>
 </template>
